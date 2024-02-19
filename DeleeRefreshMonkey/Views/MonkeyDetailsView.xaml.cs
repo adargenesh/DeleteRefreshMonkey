99077f8bd5138bbb12fbb517ebb79f03cbb83c84
User <User@LAB2-1>
2024-02-19 13:20:56 +0200

    dghfh

--- DeleeRefreshMonkey/Views/MonkeyDetailsView.xaml.cs
+++ DeleeRefreshMonkey/Views/MonkeyDetailsView.xaml.cs
@@ -1,9 +1,13 @@
+using DeleeRefreshMonkey.ViewModels;
+
 namespace DeleeRefreshMonkey.Views;
 
 public partial class MonkeyDetailsView : ContentPage
 {
-	public MonkeyDetailsView()
+	public MonkeyDetailsView(MonkeyDetailsViewModel vm)
 	{
+		
 		InitializeComponent();
+		this.BindingContext = vm;
 	}
 }
